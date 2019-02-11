a41aa7e8f092c3830da6e7aa4d4de4c8e5f5eaf9
Michael Kilcorse <kilcorse.michael92@gmail.com>
2019-02-10 21:20:42 -0500

    Update script.js
    Meets expectations criteria fulfilled

--- js/script.js
+++ js/script.js
@@ -1,52 +1,46 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-
-
 //Global scope variables
-const listStudentLi = document.getElementsByTagName("li");
+const listStudentLi = document.getElementsByClassName("student-item cf");
 const ulStudentList = document.querySelector(".student-list");
 let page;
 const studentsPerPage = 10;
+const pageDiv = document.querySelector("div.page");
+const headerDiv = document.querySelector("div.page-header cf");
 
-// the function to show only ten students per page with parameters list and showPage
-const showPage = (list, page) => {
-//setting the low and high values for each page
-const low = page * studentsPerPage;
-const high = (page + 1) * studentsPerPage;
-
-    //looping through each index of the list collection
-    for(let i = 0; i < list.length; i++){
-
-        //conditional statement to determine whether or not the index is shown
-        if(i >= low && i < high){
-          list[i].style.display = '';
-        }else{
-          list[i].style.display = 'none';
-        }
-    }
+const insertSearch = (list) => {
+    const searchbar = document.createElement("input");
+    const searchButton = document.createElement("button");
+    searchbar.type = "text";
+    searchbar.textContent = "Search Student List Here";
+    headerDiv.appendChild(searchbar);
+    headerDiv.appendChild(searchButton);
 
 }
 
 
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-const appendPageLinks = () => {
+// the function to show only ten students per page with parameters list and showPage
+const showPage = (list, page) => {
+    //setting the low and high values for each page
+    const low = page * studentsPerPage;
+    const high = (page + 1) * studentsPerPage;
+    //looping through each index of the list collection
+    for(let i = 0; i < list.length; i++){
+        //conditional statement to determine whether or not the index is shown
+        if(i >= low && i < high){
+                list[i].style.display = '';
+                }else{
+                    list[i].style.display = 'none';
+      }
+    }
+  }
+const appendPageLinks = (list) => {
   //locally scoped variables
-  const pageDiv = document.querySelector("div.page");
   const divPagination = document.createElement("div");
   const ulPagination = document.createElement("ul");
   const numberOfPages = Math.ceil((listStudentLi.length - 1) / studentsPerPage);
-
   //setting class names and appending elements to DOM
   divPagination.className = "pagination"
   pageDiv.appendChild(divPagination);
   divPagination.appendChild(ulPagination);
-
     //for loop creating buttons
       for(let i = 0; i < numberOfPages; i++){
           const pageLi = document.createElement("li");
@@ -57,20 +51,18 @@ const appendPageLinks = () => {
           pageLi.appendChild(pageA);
 
           pageA.textContent = i + 1;
+          //adding the event listener to each pageButtons
+          pageA.addEventListener("click" , (event) => {
+                event.preventDefault();
+                page = parseInt(pageA.textContent - 1);
 
-              //adding the event listener to each pageButtons
-              pageA.addEventListener("click" , (event) => {
-                  page = parseInt(pageA.textContent - 1);
+                return  showPage(list, page);
 
-                  showPage(listStudentLi, page);
 
         });
       }
    }
 
+insertSearch();
 showPage(listStudentLi, 0);
-
-appendPageLinks();
-
-
-//issues: only the first and last pagination <a> tag works; when 2-5 is clicked the appendPageLinks fails to write add links
+appendPageLinks(listStudentLi);
